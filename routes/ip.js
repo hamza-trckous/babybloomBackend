c1b166be2e9de8ad9bf65f0cdc6c661991c9701a
unknown <hamzatricks@gmail.com>
2025-01-15 15:26:31 +0100

    after final pixl

--- routes/ip.js
+++ routes/ip.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const requestIp = require("request-ip");
 
 router.get("/get-ip", (req, res) => {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  console.log("IP Address:", ip);
-  res.status(200).json({ ip });
+  let clientIp = requestIp.getClientIp(req); // Get the client's IP address
+
+  // Handle local development IPs
+  if (clientIp === "::1" || clientIp === "127.0.0.1") {
+    clientIp = "0.0.0.0"; // Replace with a placeholder or skip sending this field
+  }
+
+  console.log("Client IP Address:", clientIp);
+  res.status(200).json({ ip: clientIp });
 });
 
 module.exports = router;
