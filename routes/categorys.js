dedff2787cc6f5c9d4be677748942e5c0c1f5bcf
unknown <hamzatricks@gmail.com>
2025-06-25 14:44:38 +0100

    the last save

--- routes/categorys.js
+++ routes/categorys.js
@@ -41,6 +41,19 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+// @desc    Get One categorie
+
+router.get("/:id", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 // @route   POST /api/categories
 // @desc    Create a new category
